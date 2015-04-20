62c9ae638cd492142e0a81805fff9341756da1b5
Dylan Bernard <dtbernard11@gmail.com>
2015-04-20 18:22:34 -0400

    Setting up admin user

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None, {'fields' : ['question_text']}),
+            ('Date information', {'fields' : ['pub_date'], 'classes' : ['collapse']}),
+            ]
 
 # Register your models here.
+admin.site.register(Question, QuestionAdmin)
+
